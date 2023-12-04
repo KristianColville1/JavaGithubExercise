45563727d7bab155a236276109ae5deea6446e5d
Kristian Colville <kristiancolville1@gmail.com>
2023-12-04 21:11:48 +0000

    Refactor setting up bookshelf for user

--- src/Main.java
+++ src/Main.java
@@ -7,18 +7,19 @@ public class Main {
     public static String[][][] fourthDimension;
 
     public static void main(String[] args) {
+        setUpBasicBookshelf();
+    }
+
+    public static void setUpBasicBookshelf() {
         firstDimension = "My Bookshelf";
-
-        secondDimension = new String[]{"Fiction", "Non-Fiction"};
-
-        thirdDimension = new String[][]{
-            {"The Hobbit", "1984"},
-            {"Sapiens", "A Brief History of Time"}
+        secondDimension = new String[] { "Fiction", "Non-Fiction" };
+        thirdDimension = new String[][] {
+                { "The Hobbit", "1984" },
+                { "Sapiens", "A Brief History of Time" }
         };
-
-        fourthDimension = new String[][][]{
-            {{"The Hobbit", "J.R.R. Tolkien", "1937"}, {"1984", "George Orwell", "1949"}},
-            {{"Sapiens", "Yuval Noah Harari", "2011"}, {"A Brief History of Time", "Stephen Hawking", "1988"}}
+        fourthDimension = new String[][][] {
+                { { "The Hobbit", "J.R.R. Tolkien", "1937" }, { "1984", "George Orwell", "1949" } },
+                { { "Sapiens", "Yuval Noah Harari", "2011" }, { "A Brief History of Time", "Stephen Hawking", "1988" } }
         };
 
         System.out.println("Welcome to " + firstDimension);
