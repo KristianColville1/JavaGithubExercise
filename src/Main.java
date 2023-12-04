247c0827006386380f9d027c6d6d0bc1450b9f91
Kristian Colville <kristiancolville1@gmail.com>
2023-12-04 21:42:20 +0000

    Refactor the bookshelf and begin setting up dummy database

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,60 @@
 package src;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
-    public static String firstDimension;
-    public static String[] secondDimension;
-    public static String[][] thirdDimension;
-    public static String[][][] fourthDimension;
+
+    public static ArrayList<Object> bookShelf;
 
     public static void main(String[] args) {
         setUpBasicBookshelf();
+        printBookshelf();
     }
 
     public static void setUpBasicBookshelf() {
-        firstDimension = "My Bookshelf";
-        secondDimension = new String[] { "Fiction", "Non-Fiction" };
-        thirdDimension = new String[][] {
+        bookShelf = new ArrayList<>();
+
+        String firstDimension = "My Bookshelf";
+        bookShelf.add(firstDimension);
+
+        String[] secondDimension = new String[] { "Fiction", "Non-Fiction" };
+        bookShelf.add(secondDimension);
+
+        String[][] thirdDimension = new String[][] {
                 { "The Hobbit", "1984" },
                 { "Sapiens", "A Brief History of Time" }
         };
-        fourthDimension = new String[][][] {
-                { { "The Hobbit", "J.R.R. Tolkien", "1937" }, { "1984", "George Orwell", "1949" } },
-                { { "Sapiens", "Yuval Noah Harari", "2011" }, { "A Brief History of Time", "Stephen Hawking", "1988" } }
-        };
+        bookShelf.add(thirdDimension);
 
-        System.out.println("Welcome to " + firstDimension);
-        for (int i = 0; i < secondDimension.length; i++) {
-            System.out.println("\nCategory: " + secondDimension[i]);
-            for (int j = 0; j < thirdDimension[i].length; j++) {
-                System.out.println("Book: " + thirdDimension[i][j]);
-                System.out.println("Details: " + String.join(", ", fourthDimension[i][j]));
+        String[][][] fourthDimension = new String[][][] {
+                {
+                        { "The Hobbit", "J.R.R. Tolkien", "1937" },
+                        { "1984", "George Orwell", "1949" }
+                },
+                {
+                        { "Sapiens", "Yuval Noah Harari", "2011" },
+                        { "A Brief History of Time", "Stephen Hawking", "1988" }
+                }
+        };
+        bookShelf.add(fourthDimension);
+    }
+
+    public static void printBookshelf() {
+        System.out.println("Welcome to " + bookShelf.get(0));
+
+        String[] categories = (String[]) bookShelf.get(1);
+        String[][] books = (String[][]) bookShelf.get(2);
+        String[][][] bookDetails = (String[][][]) bookShelf.get(3);
+
+        for (int i = 0; i < categories.length; i++) {
+            System.out.println("\nCategory: " + categories[i]);
+            for (int j = 0; j < books[i].length; j++) {
+                System.out.println("Book: " + books[i][j]);
+                System.out.println("Details: " + String.join(", ", bookDetails[i][j]));
             }
         }
     }
+
+
 }
